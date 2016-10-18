96d4fc340bed8220eb6ff2e948cbc7ca9804ee10
trujunzhang <trujunzhang@gmail.com>
2016-10-19 02:22:46 +0800

    chage post_type to 'product'.

--- include/class-woocsv-product.php
+++ include/class-woocsv-product.php
@@ -298,7 +298,7 @@ class woocsv_import_product
         // TODO: DJZHANG(Save the product type,from 'simple' to 'variable')
         //save the product type
         if ($this->product["is_parent"]) {// Only for parent product.
-            wp_set_object_terms($post_id, $this->product_type, 'product_type', FALSE);
+            $this->save_parent_attributes($post_id);
         }
 
         do_action('woocsv_product_before_meta_save');
@@ -346,6 +346,60 @@ class woocsv_import_product
 
         //and return the ID
         return $post_id;
+    }
+
+    public function save_parent_attributes($post_id)
+    {
+        wp_set_object_terms($post_id, $this->product_type, 'product_type', FALSE);
+
+//        $this->insert_product_attributes($post_id)
+    }
+
+    function insert_product_attributes ($post_id, $available_attributes, $variations)
+    {
+        foreach ($available_attributes as $attribute) // Go through each attribute
+        {
+            $values = array(); // Set up an array to store the current attributes values.
+
+            foreach ($variations as $variation) // Loop each variation in the file
+            {
+                $attribute_keys = array_keys($variation['attributes']); // Get the keys for the current variations attributes
+
+                foreach ($attribute_keys as $key) // Loop through each key
+                {
+                    if ($key === $attribute) // If this attributes key is the top level attribute add the value to the $values array
+                    {
+                        $values[] = $variation['attributes'][$key];
+                    }
+                }
+            }
+
+            // Essentially we want to end up with something like this for each attribute:
+            // $values would contain: array('small', 'medium', 'medium', 'large');
+
+            $values = array_unique($values); // Filter out duplicate values
+
+            // Store the values to the attribute on the new post, for example without variables:
+            // wp_set_object_terms(23, array('small', 'medium', 'large'), 'pa_size');
+            wp_set_object_terms($post_id, $values, 'pa_' . $attribute);
+        }
+
+        $product_attributes_data = array(); // Setup array to hold our product attributes data
+
+        foreach ($available_attributes as $attribute) // Loop round each attribute
+        {
+            $product_attributes_data['pa_'.$attribute] = array( // Set this attributes array to a key to using the prefix 'pa'
+
+                'name'         => 'pa_'.$attribute,
+                'value'        => '',
+                'is_visible'   => '1',
+                'is_variation' => '1',
+                'is_taxonomy'  => '1'
+
+            );
+        }
+
+        update_post_meta($post_id, '_product_attributes', $product_attributes_data); // Attach the above array to the new posts meta data key '_product_attributes'
     }
 
     public function save_tags($post_id)
